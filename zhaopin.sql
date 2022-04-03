923a5bb433582b5af916ea9fc2f09b205ceb1c7d
a1016669075 <60488790+a1016669075@users.noreply.github.com>
2022-04-03 11:23:50 +0200

    commit

--- zhaopin.sql
+++ zhaopin.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50525
 File Encoding         : 65001
 
-Date: 2022-03-28 16:16:26
+Date: 2022-04-01 18:08:16
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,22 +20,27 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `chat`;
 CREATE TABLE `chat` (
-  `_id` int(11) NOT NULL,
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
   `_from` varchar(255) DEFAULT NULL,
   `_to` varchar(255) DEFAULT NULL,
   `chat_id` varchar(255) DEFAULT NULL,
   `content` varchar(255) DEFAULT NULL,
-  `read` varchar(255) DEFAULT NULL,
+  `_read` varchar(255) DEFAULT NULL,
   `create_time` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of chat
+-- ----------------------------
+INSERT INTO `chat` VALUES ('1', '0', '1', '0_1', null, null, null);
 
 -- ----------------------------
 -- Table structure for userinfo
 -- ----------------------------
 DROP TABLE IF EXISTS `userinfo`;
 CREATE TABLE `userinfo` (
-  `_id` int(11) NOT NULL,
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
@@ -45,4 +50,10 @@ CREATE TABLE `userinfo` (
   `company` varchar(255) DEFAULT NULL,
   `salary` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of userinfo
+-- ----------------------------
+INSERT INTO `userinfo` VALUES ('0', '111', '111', 'laoban', '头像1', '11', '44', '22', '33');
+INSERT INTO `userinfo` VALUES ('1', '222', '222', 'dashen', '头像2', '22', '22', null, null);
